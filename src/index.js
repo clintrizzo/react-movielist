fafc4473fdf3edb782ac1ec147e1ef3c9226c0ee
Clint <maxeva02@gmail.com>
2021-07-21 14:54:55 -0700

    set up root file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('movieList')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return <h1>Movie List</h1>
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
