8c9958cd0431b8c6021fa156d9981371e2cc423c
Clint <maxeva02@gmail.com>
2021-07-25 15:47:22 -0700

    updated webpack file with correct builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-const HtmlPlugin = new HtmlWebPackPlugin ({
-    template: './src/index.html',
-    filename: './index.html'
-})
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: '"babel-loader',
-                },
-            },
-        ],
-    },
-    plugins: [HtmlPlugin],
-}
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+};
