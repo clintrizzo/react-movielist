70ec3ef0d75ae2a91c331ac98958e0ca3ce405e5
Clint <maxeva02@gmail.com>
2021-07-29 14:29:27 -0700

    started working on creating a multiple page project from tutorial

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const navbar= () => {
+    return (
+        <div>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/moviesPage">Movies</Link>
+            </li>
+            <li>
+                <Link to="/tvPage">TV Shows</Link>
+            </li>
+        </div>
+    )
+}
+ export default navbar;
