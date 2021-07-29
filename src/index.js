028dd6a37e069ec4b8349b8c8b87b1d1f496527b
Clint <maxeva02@gmail.com>
2021-07-29 14:30:29 -0700

    set up the router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import List from './containers/movieList'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "src/components/navbar/navbar.js"
+import homePage from "src/pages/homePage"
+import moviesPage from "src/pages/moviePage"
+import tvPage from "src/pages/tvPage"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const App = () => {
+function App() {
     return (
-    <div className='container-fluid'>
-        <h1>Movie List</h1>
-    <List />
-    </div>
-    )
-};
+        <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={homePage} />
+          <Route path='/moviesPage' component={moviesPage} />
+          <Route path='/tvPage' component={tvPage} />
+        </Switch>
+      </Router>
+    );
+  }
 
 ReactDOM.render(<App />, document.getElementById('root'));
