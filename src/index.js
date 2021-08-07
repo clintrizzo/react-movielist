cf21719873bb7c26a1db9f146be30cd0bf31c711
Clint <maxeva02@gmail.com>
2021-08-07 15:21:04 -0700

    added marvel faviorites to the navbar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
 import Home from '../src/pages/homePage'
 import moviesPage from '../src/pages/moviePage'
 import tvPage from '../src/pages/tvPage'
+import marvelPage from '../src/pages/marvelPage'
 
 function App() {
     return (
@@ -23,11 +24,15 @@ function App() {
                     <Link to='/tvPage' style={{ padding: 10 }}>
                         TV Faviorites
                     </Link>
+                    <Link to='/marvelPage' style={{ padding: 10 }}>
+                        Marvel Faviorites
+                    </Link>
                 </nav>
             <Switch>
                 <Route exact path='/' exact component={Home} />
                 <Route path='/moviePage' component={moviesPage} />
                 <Route path='/tvPage' component={tvPage} />
+                <Route path='/marvelPage' component={marvelPage} />
             </Switch>
             </div>
         </Router>
