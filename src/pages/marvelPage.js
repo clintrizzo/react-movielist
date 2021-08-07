b2238412bd50465577add1aa4abddb1d00f46c0b
Clint <maxeva02@gmail.com>
2021-08-07 15:21:34 -0700

    brought in json card info

---
+++ src/pages/marvelPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import List from '../containers/marvelList'
+
+function moviesPage () {
+    return (
+        <div>
+            <center>
+            <h1><header>Marvel List</header></h1>
+            </center>
+            <List />
+        </div>
+    )
+}
+export default moviesPage;
